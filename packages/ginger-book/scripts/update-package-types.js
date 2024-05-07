cc46cceb2fff54c2be9f0ccb4e6ddc743e9b4ed2
Ginger Society Admin <admin@gingersociety.org>
2024-05-07 12:55:38 +0530

    chore: minor

--- packages/ginger-book/scripts/update-package-types.js
+++ packages/ginger-book/scripts/update-package-types.js
@@ -1,16 +1,18 @@
 import { preparePackageJsonForPublish } from "./package-types-helpers.js";
 import fs from "fs";
 
-const pkgJson = JSON.parse(fs.readFileSync("./packages/ladle/package.json"));
+const pkgJson = JSON.parse(
+  fs.readFileSync("./packages/ginger-book/package.json"),
+);
 // write out old package.json to a temp file that won't be published
 fs.writeFileSync(
-  "./packages/ladle/backup-package.json",
+  "./packages/ginger-book/backup-package.json",
   JSON.stringify(pkgJson, null, 2),
 );
 // update existing package.json
 preparePackageJsonForPublish(pkgJson);
 // write updates to package.json
 fs.writeFileSync(
-  "./packages/ladle/package.json",
+  "./packages/ginger-book/package.json",
   JSON.stringify(pkgJson, null, 2),
 );
