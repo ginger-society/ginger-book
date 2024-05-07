d0a0ac64ebe2fa35435136420058d4e6098a997b
Ginger Society Admin <admin@gingersociety.org>
2024-05-07 12:51:11 +0530

    chore: minor

--- release.sh
+++ release.sh
@@ -8,11 +8,11 @@ echo "Build @ginger-society/ginger-book"
 turbo run build --filter=@ginger-society/ginger-book
 
 echo "Update package.json"
-node ./packages/ladle/scripts/update-package-types.js
+node ./packages/ginger-book/scripts/update-package-types.js
 
 echo "Generate Types":
 pnpm --filter @ginger-society/ginger-book types
 
 
 echo "Revert package.json"
-node ./packages/ladle/scripts/revert-package-types.js
+node ./packages/ginger-book/scripts/revert-package-types.js
